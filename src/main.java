f4fd72cc824042ede66b2cd5140c6e5b6374f103
Grigoriy <grigoriykosarev@gmail.com>
2017-06-27 15:11:10 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Grigoriy on 27.06.2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Как я проведу лето 2017 ... незнаю :)");
+    }
+}

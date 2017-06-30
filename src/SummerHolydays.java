196f212656e888f1af19cc5958256b00f9722192
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-06-30 16:49:36 +0300

    Виправлення помилок.
    Не можна називати класс та метод однаково; Назва класу має бути написаною з великої літери.

---
+++ src/SummerHolydays.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by admin on 30.06.2017.
+ */
+public class SummerHolydays {
+}
--- src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Grigoriy on 27.06.2017.
- */
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Как я проведу лето 2017 ... незнаю :)");
-    }
-}
